f3008cb0cbcb347fc83833f5d610bc8a46880db8
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-06-27 16:45:16 +0545

    feat: min max find

---
+++ day15/script.js
@@ -0,0 +1,41 @@
+const array1 = [10, 1000, 0];
+
+const findMaximumMinimumValue = (arr) => {
+   let min = arr[0];
+   let max = arr[0];
+   arr.forEach((a) => {
+
+      if (a < min) {
+         min = a;
+      }
+
+      if (a > max) {
+         max = a;
+      }
+   })
+
+   // for (let i = 1; i < arr.length; i++) {
+   //    if (arr[i] < min) {
+   //       min = arr[i];
+   //    }
+
+   //    if (arr[i] > max) {
+   //       max = arr[i];
+   //    }
+   // }
+   // }
+   return {
+      min,
+      max
+   }
+}
+
+// const num = findMaximumMinimumValue(array1);
+
+// console.log(`${array1}`)
+
+// document.write(`${num.min} is the minimum num`)
+// document.write(`${num.max} is the max num`)
+
+
+
